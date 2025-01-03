1f5e3439fa0919b4f10d22b034eedf0ea7b20c30
HazeFury <mab.dev69@gmail.com>
2025-01-03 11:17:22 +0100

    erreur dans les tests du I9 et du A1

--- algos/intermediate/I9/I9.test.js
+++ algos/intermediate/I9/I9.test.js
@@ -3,23 +3,19 @@
 const trierLettres = require("./I9");
 
 describe("trierLettres", () => {
-	test("trie correctement les lettres de chaque mot", () => {
-		expect(trierLettres("Bonjour tout le monde")).toBe("Bjnooru ottu el demno");
-	});
+  test("trie correctement les lettres de chaque mot", () => {
+    expect(trierLettres("Bonjour tout le monde")).toBe("Bjnooru ottu el demno");
+  });
 
-	test("fonctionne avec une seule lettre par mot", () => {
-		expect(trierLettres("A B C")).toBe("A B C");
-	});
+  test("fonctionne avec une seule lettre par mot", () => {
+    expect(trierLettres("A B C")).toBe("A B C");
+  });
 
-	test("fonctionne avec des caractères spéciaux et des espaces", () => {
-		expect(trierLettres("Salut ! Ça va ?")).toBe("Saltu ! Ça av ?");
-	});
+  test("fonctionne avec une chaîne vide", () => {
+    expect(trierLettres("")).toBe("");
+  });
 
-	test("fonctionne avec une chaîne vide", () => {
-		expect(trierLettres("")).toBe("");
-	});
-
-	test("fonctionne avec des mots répétitifs", () => {
-		expect(trierLettres("abc cba bac")).toBe("abc abc abc");
-	});
+  test("fonctionne avec des mots répétitifs", () => {
+    expect(trierLettres("abc cba bac")).toBe("abc abc abc");
+  });
 });
